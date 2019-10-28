45c1dc93b99863e9210ce1369ac9bf03890139db
Francisco Rodriguez <francisco.rodriguez@epn.edu.ec>
2019-10-28 16:01:51 -0500

    Clase operaciones

---
+++ mi carpeta/04-condicionales.ts
@@ -0,0 +1,81 @@
+//  04-condicionales.ts
+const casado = true;
+if(casado == true){
+    console.log("Si estoy casado");
+} else {
+    console.log("No estoy casado");
+}
+
+if(casado== false){   // (Expresion => boolean)
+    console.log("No estoy casado");
+} else {
+    console.log("Si estoy casado");
+}
+
+if(casado != true){
+    console.log("Si estoy casado");
+} else {
+    console.log("No estoy casado");
+}
+
+f(casado != false){
+    console.log("Si estoy casado");
+} else {
+    console.log("No estoy casado");
+}
+
+const tengoMosa = false
+const estaCasado =  true == true;  // expresion
+
+
+const soyMosero = tengoMosa == true; //
+
+const casadoYMosero =  casado == true && tengoMosa == true;
+if(casado == true && tengoMosa == true){
+    console.log("Casado y mosero")
+} else{
+     console.log("O casado o Mosero o Nada")
+}
+
+
+
+
+
+// casdoMosero  <=>  c==true && b== true && d==true
+
+// AND || ANSTERS
+// TRUE y TRUE = TRUE
+// TRUE y FALSE = FALSE
+// FALSE y TRUE = FALSE
+// FALSE y FALSE = FALSE
+
+// OR  -> || -> PIPES
+// TRUE y TRUE = TRUE
+// TRUE y FALSE = TRUE
+// FALSE y TRUE = TRUE
+// FALSE y FALSE = FALSE
+
+/*
+1.	Ingrese un numero
+2.	Ingrese otro numero 
+3.	Escoja la operación (suma, resta multiplicaion y division)
+4.	Primer número igual a la variable a 
+5.	Segundo número igual a la variable b
+6.	Sumar restar multiplicar o dividir “a” y “b”
+
+*/
+
+// apago la primera alarma me hago 
+// bolita y lloro hasta la segunda 
+// alarma y me levanto
+// celularDescargado == true
+// amaneceMuerto  == true
+// estaActivadaLaAlarma  !== true
+// estaEnModoSilencio  == true
+// const seAtrasoAClase = celularDescargado == ||true amaneceMuerto  == true ||estaActivadaLaAlarma  !== true || estaEnModoSilencio  == true
+
+// if(const == true) {
+    console.log("No llego a clases");
+} else{
+      console.log()   
+     }
