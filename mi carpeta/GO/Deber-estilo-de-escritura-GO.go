c148a1041af7479919c105a037c8ec237913ea84
XERF99 <francisco.rodriguez@epn.edu.ec>
2019-10-28 19:45:30 -0500

    Estilos de escritura GO

---
+++ mi carpeta/GO/Deber-estilo-de-escritura-GO.go
@@ -0,0 +1,18 @@
+/*
+Autor: Francisco Rodriguez
+Fecha actualización: 28/10/2019
+Descripción: Estilo de escritura que se utiliza en el lenguaje GO.
+
+Dentro del lenguaje de GO se utiliza principalmente un tipo de "nomenclatura":
+
+Esta consiste en que la palabra o variable este solo con letras minusculas y separadas por un espacio vacio
+Ejemplo:
+variable paginas datos ; todos las letras que conforman la palabra tienen que estar escritas en minusculas
+y separadas por un espacio.
+
+
+Fuente:
+https://golang.org/doc/
+https://golang.org/doc/effective_go.html#interface-names
+
+*/
