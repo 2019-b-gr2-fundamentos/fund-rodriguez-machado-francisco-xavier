fd7032879e5ce47680ee491d2c818fe81476a731
Francisco Rodriguez <francisco.rodriguez@epn.edu.ec>
2019-12-18 17:52:20 -0500

    Matrices

---
+++ mi carpeta/funciones/04-Matrices.js
@@ -0,0 +1,83 @@
+var arregloMatriz = [
+    [1, 2],
+    [3, 4, 5],
+    [6, 7, 8, 6],
+    [9],
+    [],
+];
+function compararMatriz(matrizUno, matrizDos) {
+    var matrizUnoPrimeraDimension = obtenerPrimeraDimension(matrizUno);
+    var matrizUnoSegundaDimension = obtenerSegundaDimension(matrizUno);
+    var matrizDosPrimeraDimension = obtenerPrimeraDimension(matrizDos);
+    var matrizDosSegundaDimension = obtenerSegundaDimension(matrizDos);
+    console.log(matrizUnoPrimeraDimension);
+    console.log(matrizUnoSegundaDimension);
+    console.log(matrizDosPrimeraDimension);
+    console.log(matrizDosSegundaDimension);
+    return true;
+}
+function obtenerPrimeraDimension(matrizUno) {
+    // VALIDACIONES
+    var esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
+    if (esValido) {
+        var primeraDimensionArreglo = matrizUno.length;
+        return primeraDimensionArreglo;
+    }
+    else {
+        return false;
+    }
+}
+function obtenerSegundaDimension(matrizUno) {
+    var esValido = verificarTodosLosElementosDeUnArregloSonArreglo(matrizUno);
+    if (esValido) {
+        var longitudActualMaxima = 0; // Auxiliar
+        var longitudActualMinima = -1; // Auxiliar
+        for (var i = 0; i < matrizUno.length; i++) {
+            var elementoActual = matrizUno[i]; // arreglo
+            var longitudActual = elementoActual.length; // segunda dimension
+            if (longitudActualMaxima < longitudActual) {
+                longitudActualMaxima = longitudActual;
+            }
+            if (longitudActualMinima == -1) {
+                longitudActualMinima = longitudActual;
+            }
+            else {
+                if (longitudActual < longitudActualMinima) {
+                    longitudActualMinima = longitudActual;
+                }
+            }
+        }
+        if (longitudActualMaxima != longitudActualMinima) {
+            return false;
+        }
+        else {
+            return matrizUno[0].length;
+        }
+    }
+    else {
+        return false;
+    }
+}
+function verificarTodosLosElementosDeUnArregloSonArreglo(arreglo) {
+    for (var i = 0; i < arreglo.length; i++) {
+        var elementoActual = arreglo[i];
+        var esUnArreglo = typeof elementoActual == 'object' &&
+            elementoActual.indexOf; // Truty
+        if (!esUnArreglo) {
+            return false;
+        }
+    }
+    return true;
+}
+function main() {
+    var matrizUno = [
+        [1, 2],
+        [3]
+    ];
+    var matrizDos = [
+        [1, 2],
+        [3, 4],
+    ];
+    compararMatriz(matrizUno, matrizDos);
+}
+main();
