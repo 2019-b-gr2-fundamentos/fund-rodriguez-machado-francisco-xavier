569386d9aafa19ac3d5d0bc99ef02f6b8cad8f67
XERF99 <francisco.rodriguez@epn.edu.ec>
2020-01-09 16:52:11 -0500

    librerias actualizadas

--- mi carpeta/05-proyecto/index.ts
+++ mi carpeta/05-proyecto/index.ts
@@ -7,26 +7,29 @@ import * as sumaLibreria from 'librerias-f-r-m/lib/suma'
 const respuesta1 = sumaLibreria(1,2);
 console.log('La respuesta es: ',respuesta1);
 
-import * as restaLibreria from 'calculadora_fr/resta'
+import * as restaLibreria from 'calculadora_fr/lib/resta'
 const respuesta2 = restaLibreria(20,14);
 console.log('La respuesta es: ',respuesta2);
 
-import * as multiplicacionLibreria from 'calculadora_fr/multiplicacion'
+import * as multiplicacionLibreria from 'calculadora_fr/lib/multiplicacion'
 const respuesta3 = multiplicacionLibreria(5,8);
 console.log('La respuesta es: ',respuesta3);
 
-import * as divisionLibreria from 'calculadora_fr/division'
+import * as divisionLibreria from 'calculadora_fr/lib/division'
 const respuesta4 = divisionLibreria(5,8);
 console.log('La respuesta es: ',respuesta4);
 
-import * as ivaLibreria from 'calculadora_fr/iva'
+import * as ivaLibreria from 'calculadora_fr/lib/iva'
 const respuesta5 = ivaLibreria(510);
 console.log('La respuesta es: ',respuesta5);
 
-import * as PromedioDosNumerosLibreria from 'calculadora_fr/PromedioDosNumeros'
+import * as PromedioDosNumerosLibreria from 'calculadora_fr/lib/PromedioDosNumeros'
 const respuesta6 = PromedioDosNumerosLibreria(20,10);
 console.log('La respuesta es: ',respuesta6);
 
-import * as VolumenCuboLibreria from 'calculadora_fr/VolumenCubo'
+import * as VolumenCuboLibreria from 'calculadora_fr/lib/VolumenCubo'
 const respuesta7 = VolumenCuboLibreria(20);
 console.log('La respuesta es: ',respuesta7);
+
+import * as PI from 'calculadora_fr/lib/pi'
+console.log('la respuesta es' , PI)
